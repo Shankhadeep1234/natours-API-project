cc28edbdda3538ff03e40ad9615a44d3adb7d4cd
Shankhadeep Bhadra <shankhadeepraj@gmail.com>
2019-12-20 17:53:40 +0530

    made API filtering better with req.query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,18 @@ const Tour = require('../models/tourModels');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find({});
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    const tours = await Tour.find(queryObj);
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
     res.status(200).json({
       status: 'success',
       results: tours.length,
